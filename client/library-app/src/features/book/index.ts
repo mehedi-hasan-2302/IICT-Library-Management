372b13973c81b3e20da02cdc0e1a0b045136e313
gfor-gour <talukdargour.bd@gmail.com>
2024-06-06 15:16:50 +0600

    book loan system completed

--- client/library-app/src/features/book/index.ts
+++ client/library-app/src/features/book/index.ts
@@ -1,6 +1,7 @@
 import { BookInformation } from "./components/BookInformation/BookInfornation";
 import { BookCard } from "./components/BookCard/BookCard";
 import { BookCarousel } from "./components/BookCarousel/BookCarousel";
+import { LoanBookModal } from "./components/LoanBookModal/LoanBookModal";
 
 
-export {BookInformation, BookCard, BookCarousel};
+export {BookInformation, BookCard, BookCarousel, LoanBookModal};
